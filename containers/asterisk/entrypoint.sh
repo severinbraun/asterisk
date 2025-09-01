028380b926e4f9a1fb8564884f179f8e4a364dc5
severinbraun <severin.braun@me.com>
2025-09-01 19:50:05 +0200

    Update entrypoint.sh

--- containers/asterisk/entrypoint.sh
+++ containers/asterisk/entrypoint.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 set -eu
 
-# Verzeichnisse anlegen + Eigentümer setzen (Docker verliert /var/run beim Neustart)
-mkdir -p /var/run/asterisk /var/log/asterisk /var/spool/asterisk /var/lib/asterisk
-chown -R asterisk:asterisk /var/run/asterisk /var/log/asterisk /var/spool/asterisk /var/lib/asterisk
+# Laufzeit-Verzeichnisse/Ownership absichern (idempotent)
+mkdir -p /run/asterisk /var/log/asterisk /var/spool/asterisk
+chown -R asterisk:asterisk /run/asterisk /var/log/asterisk /var/spool/asterisk /var/lib/asterisk || true
 
-# Sicherstellen, dass das Modulverzeichnis existiert (kommt mit dem Paket 'asterisk')
-if [ ! -d /usr/lib/asterisk/modules ]; then
-  echo "ERROR: /usr/lib/asterisk/modules fehlt – ist das Paket 'asterisk' installiert?"
-  ls -la /usr/lib/asterisk || true
-  exit 1
-fi
-
-# Als 'asterisk' starten (kein root zur Laufzeit)
-exec su-exec asterisk:asterisk /usr/sbin/asterisk -f -U asterisk -G asterisk -C /etc/asterisk/asterisk.conf -vvv
+# Asterisk im Vordergrund starten
+exec /usr/sbin/asterisk -f -U asterisk -G asterisk -vvv
