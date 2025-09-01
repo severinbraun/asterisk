73a0d5bfd1dc62af03ca96ddab709e15830f87e9
severinbraun <severin.braun@me.com>
2025-09-01 20:30:37 +0200

    Update entrypoint.sh

--- containers/asterisk/entrypoint.sh
+++ containers/asterisk/entrypoint.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
+# POSIX sh: ersetzt Platzhalter aus ENV in /etc/asterisk und startet Asterisk
 set -eu
 
-# Laufzeit-Verzeichnisse/Ownership absichern (idempotent)
-mkdir -p /run/asterisk /var/log/asterisk /var/spool/asterisk
-chown -R asterisk:asterisk /run/asterisk /var/log/asterisk /var/spool/asterisk /var/lib/asterisk || true
+tmpl_dir="/etc/asterisk.tmpl"
+dst_dir="/etc/asterisk"
 
-# Asterisk im Vordergrund starten
-exec /usr/sbin/asterisk -f -U asterisk -G asterisk -vvv
+if [ ! -d "$dst_dir" ]; then
+  mkdir -p "$dst_dir"
+fi
+
+# Dateien rendern (Platzhalter: __FOO__)
+for f in asterisk.conf extensions.conf logger.conf modules.conf pjsip.conf rtp.conf sip.conf; do
+  src="$tmpl_dir/$f"
+  dst="$dst_dir/$f"
+  if [ -f "$src" ]; then
+    sed \
+      -e "s|__DOORBIRD_USER__|${DOORBIRD_USER}|g" \
+      -e "s|__DOORBIRD_PASS__|${DOORBIRD_PASS}|g" \
+      -e "s|__HA_USER__|${HA_USER}|g" \
+      -e "s|__HA_PASS__|${HA_PASS}|g" \
+      -e "s|__RTP_START__|${RTP_START}|g" \
+      -e "s|__RTP_END__|${RTP_END}|g" \
+      -e "s|__LOG_LEVEL__|${ASTERISK_LOG_LEVEL}|g" \
+      "$src" > "$dst"
+  fi
+done
+
+# Log-Verzeichnis sicherstellen
+if [ ! -d /var/log/asterisk ]; then
+  mkdir -p /var/log/asterisk
+fi
+
+# Asterisk starten; CMD-Args (z.B. "-f") werden durchgereicht
+exec /usr/sbin/asterisk "$@"
